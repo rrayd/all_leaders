dcde6065df7fa6c67a6f49bd5313410120fd7005
Dmitry Raikov <design@aio.media>
2017-09-07 16:50:06 +0500

    created top show

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,8 +3,17 @@
 // al_app
 function al_app () {
 
-	// форма регистрации
-	return false;
+	var al_header = document.getElementById('al-header');
+
+	function header_height() {
+		al_header.style.height = window.innerHeight + 'px';
+	};
+	header_height();
+
+	function global_resize() {
+		header_height();
+	};
+	window.onresize = global_resize;
 
 };
 
