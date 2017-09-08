5ad892dad6b520d67bc2feb14621e0cbd55686d8
Dmitry Raikov <design@aio.media>
2017-09-08 16:38:23 +0500

    all styles connected

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,9 +4,12 @@
 function al_app () {
 
 	var al_header = document.getElementById('al-header');
+	var al_contacts = document.getElementById('contacts');
 
 	function header_height() {
 		al_header.style.height = window.innerHeight + 'px';
+		al_contacts.style.height = window.innerHeight - window.innerHeight/4 + 'px';
+		al_contacts.style.paddingTop = window.innerHeight/4 + 'px';
 	};
 	header_height();
 
