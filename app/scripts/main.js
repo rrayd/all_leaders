8bb86a0a06034efbd83ca571e27f3f490951fd8f
Dmitry Raikov <design@aio.media>
2017-09-10 14:31:59 +0500

    anchors smooth scroll

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,39 @@
 'use strict';
+$( document ).ready(function() {
+	
+	console.log( "ready!" );
+	
+	// al_app
+	function al_app () {
 
-// al_app
-function al_app () {
+		var al_header = document.getElementById('al-header');
+		var al_contacts = document.getElementById('contacts');
 
-	var al_header = document.getElementById('al-header');
-	var al_contacts = document.getElementById('contacts');
-
-	function header_height() {
-		al_header.style.height = window.innerHeight + 'px';
-		al_contacts.style.height = window.innerHeight - window.innerHeight/4 + 'px';
-		al_contacts.style.paddingTop = window.innerHeight/4 + 'px';
-	};
-	header_height();
-
-	function global_resize() {
+		function header_height() {
+			al_header.style.height = window.innerHeight + 'px';
+			al_contacts.style.height = window.innerHeight - window.innerHeight/4 + 'px';
+			al_contacts.style.paddingTop = window.innerHeight/4 + 'px';
+		};
 		header_height();
+
+		function global_resize() {
+			header_height();
+		};
+		window.onresize = global_resize;
+
 	};
-	window.onresize = global_resize;
 
-};
+	al_app();
 
-al_app();
+	$('a[href^="#"]').on('click', function(event) {
+		var target = $(this.getAttribute('href'));
+		if( target.length ) {
+			event.preventDefault();
+			$('html, body').stop().animate({
+				scrollTop: target.offset().top
+			}, 1000);
+		}
+	});
+
+
+});
